1fb32620df933c2360f68bfc500fc48d5be5aaca
ChefDK User <chef@ip-10-183-217-10.ec2.internal>
2016-06-23 14:16:45 +0000

    Updated after adding template

--- apache/recipes/server.rb
+++ apache/recipes/server.rb
@@ -1,9 +1,8 @@
 package 'httpd' do
 	action :install
 end
-file '/var/www/html/index.html' do
-	content "<h1>Hello World! Colivares here</h1><hr><br><h2>IPADDRESS: #{node['ipaddress']}</h2><hr><br><h2>HOSTNAME : #{node['hostname']}</h2>"
-                 
+template '/var/www/html/index.html' do
+                source 'index.html.erb'
 end
 service 'httpd' do
 	action [ :enable, :start ]
