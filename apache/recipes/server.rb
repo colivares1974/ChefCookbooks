6ac4dc4f40e5fe2da6314a9b62d4c55f34051a40
ChefDK User <chef@ip-10-183-217-10.ec2.internal>
2016-06-23 13:31:55 +0000

    subido con cambios IP y Hostname en WebServer

--- apache/recipes/server.rb
+++ apache/recipes/server.rb
@@ -2,7 +2,8 @@ package 'httpd' do
 	action :install
 end
 file '/var/www/html/index.html' do
-	content '<h1>Hello World!</h1>'
+	content "<h1>Hello World! Colivares here</h1><hr><br><h2>IPADDRESS: #{node['ipaddress']}</h2><hr><br><h2>HOSTNAME : #{node['hostname']}</h2>"
+                 
 end
 service 'httpd' do
 	action [ :enable, :start ]
