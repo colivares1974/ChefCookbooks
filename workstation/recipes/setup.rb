ef91f05d54c7681fa77c3d83ba3ff1d4bdb7cddb
ChefDK User <chef@ip-10-183-217-10.ec2.internal>
2016-06-23 13:08:56 +0000

    update changes with ahoi command

--- workstation/recipes/setup.rb
+++ workstation/recipes/setup.rb
@@ -4,3 +4,13 @@ end
 package 'git' do
 	action :install
 end
+
+file '/etc/motd' do 
+   content "property of Colivares
+
+HOSTNAME : #{node['ipaddress']}
+IP : #{node['ipaddress']}
+MEM : #{node['memory']['total']}
+CPU : #{node['cpu']['0']['mhz']} MHz
+"
+end
